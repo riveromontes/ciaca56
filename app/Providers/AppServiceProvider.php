ae2936096566caf41a6bf5ae616f27a43ced77f0
Luis E. Rivero <riveromontes@gmail.com>
2018-06-04 23:08:54 -0400

    Se configuraron las bases de datos y se crearon los seeders

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+//ESTO LO HACEMOS PARA PODER USAR Schema::defaultStringLength
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+      //ESTO LO HACEMOS PARA DEFINIR LA LONGITUD DE LOS CAMPOS UNICOS EN LA BASE DE DATOS
+        Schema::defaultStringLength(120);
     }
 
     /**
