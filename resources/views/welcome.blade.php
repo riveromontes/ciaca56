30b4ac66f3190fd681d7fe97338aea47cf6e9593
Luis E. Rivero <riveromontes@gmail.com>
2018-06-09 05:44:00 -0400

    Modificación del welcome y un error que había en las rutas referente al middleware (mal escrito)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Roles y Permisos
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://google.com.ve">Google</a>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -25,79 +25,79 @@ Route::middleware(['auth'])->group(function(){
 
       //7 rutas para roles
       //Route::post(RUTA)->name(NOMBRE_DE_RUTA)
-        //->middlewere(PERMISO);
+        //->middleware(PERMISO);
       Route::post('roles/store', 'RoleController@store')->name('roles.store')
-        ->middlewere('permission:roles.create');
+        ->middleware('permission:roles.create');
 
       Route::get('roles', 'RoleController@index')->name('roles.index')
-        ->middlewere('permission:roles.index');
+        ->middleware('permission:roles.index');
 
       Route::get('roles/create', 'RoleController@create')->name('roles.create')
-        ->middlewere('permission:roles.create');
+        ->middleware('permission:roles.create');
 
     //abajo entre llaves estamos pasando un parametro que será esperado
       Route::put('roles/{role}', 'RoleController@update')->name('roles.update')
-        ->middlewere('permission:roles.edit');
+        ->middleware('permission:roles.edit');
 
       Route::get('roles/{role}', 'RoleController@show')->name('roles.show')
-        ->middlewere('permission:roles.show');
+        ->middleware('permission:roles.show');
 
       Route::delete('roles/{role}', 'RoleController@destroy')->name('roles.destroy')
-        ->middlewere('permission:roles.destroy');
+        ->middleware('permission:roles.destroy');
 
       Route::get('roles/{role}/edit', 'RoleController@edit')->name('roles.edit')
-        ->middlewere('permission:roles.edit');
+        ->middleware('permission:roles.edit');
 
 
 
       //7 rutas para products
       //Route::post(RUTA)->name(NOMBRE_DE_RUTA)
-        //->middlewere(PERMISO);
+        //->middleware(PERMISO);
       Route::post('products/store', 'ProductController@store')->name('products.store')
-        ->middlewere('permission:products.create');
+        ->middleware('permission:products.create');
 
       Route::get('products', 'ProductController@index')->name('products.index')
-        ->middlewere('permission:products.index');
+        ->middleware('permission:products.index');
 
       Route::get('products/create', 'ProductController@create')->name('products.create')
-        ->middlewere('permission:products.create');
+        ->middleware('permission:products.create');
 
     //abajo entre llaves estamos pasando un parametro que será esperado
       Route::put('products/{role}', 'ProductController@update')->name('products.update')
-        ->middlewere('permission:products.edit');
+        ->middleware('permission:products.edit');
 
       Route::get('products/{role}', 'ProductController@show')->name('products.show')
-        ->middlewere('permission:products.show');
+        ->middleware('permission:products.show');
 
       Route::delete('products/{role}', 'ProductController@destroy')->name('products.destroy')
-        ->middlewere('permission:products.destroy');
+        ->middleware('permission:products.destroy');
 
       Route::get('products/{role}/edit', 'ProductController@edit')->name('products.edit')
-        ->middlewere('permission:products.edit');
+        ->middleware('permission:products.edit');
 
 
 
       //rutas para users
       //Route::post(RUTA)->name(NOMBRE_DE_RUTA)
-        //->middlewere(PERMISO);
+        //->middleware(PERMISO);
       Route::get('users', 'UserController@index')->name('users.index')
-        ->middlewere('permission:users.index');
+        ->middleware('permission:users.index');
 
     //abajo entre llaves estamos pasando un parametro que será esperado
       Route::put('users/{role}', 'UserController@update')->name('users.update')
-        ->middlewere('permission:users.edit');
+        ->middleware('permission:users.edit');
 
       Route::get('users/{role}', 'UserController@show')->name('users.show')
-        ->middlewere('permission:users.show');
+        ->middleware('permission:users.show');
 
       Route::delete('users/{role}', 'UserController@destroy')->name('users.destroy')
-        ->middlewere('permission:users.destroy');
+        ->middleware('permission:users.destroy');
 
       Route::get('users/{role}/edit', 'UserController@edit')->name('users.edit')
-        ->middlewere('permission:users.edit');
+        ->middleware('permission:users.edit');
 
 
 
 
-        
+
 });
