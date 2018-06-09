4e9e903732661536cafeebb75a50b3f42703011f
Luis E. Rivero <riveromontes@gmail.com>
2018-06-09 05:26:06 -0400

    Fueron creadas todas las rutas necesarias para roles, productos y usuarios

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,30 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 //routes
+
+Route::middleware(['auth'])->group(function(){
+      //roles
+      //Route::post(RUTA)->name(NOMBRE_DE_RUTA)
+        //->middlewere(PERMISO);
+      Route::post('roles/store')->name('roles.store')
+        ->middlewere('permission:roles.create');
+
+      Route::get('roles')->name('roles.index')
+        ->middlewere('permission:roles.index');
+
+      Route::get('roles/create')->name('roles.create')
+        ->middlewere('permission:roles.create');
+
+    //abajo entre llaves estamos pasando un parametro que será esperado
+      Route::put('roles/{role}')->name('roles.update')
+        ->middlewere('permission:roles.edit');
+
+      Route::get('roles/{role}')->name('roles.show')
+        ->middlewere('permission:roles.show');
+
+      Route::delete('roles/{role}')->name('roles.destroy')
+        ->middlewere('permission:roles.destroy');
+        
+      Route::get('roles/{role}/edit')->name('roles.edit')
+        ->middlewere('permission:roles.edit');
+});
