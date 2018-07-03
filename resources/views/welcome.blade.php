635a2f895989e69c14d3ed47b47b4cbd88ac522e
Luis E. Rivero <riveromontes@gmail.com>
2018-07-02 21:57:21 -0400

    Modificadas algunas vistas para poner Sistema Ciaca en vez de Roles y Permisos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Sistema Ciaca</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Roles y Permisos
+                    Sistema Ciaca
                 </div>
 
                 <div class="links">
