ea471f8b6b02a9dffbcb07c0abc7212c9d251e7b
Luis E. Rivero <riveromontes@gmail.com>
2018-07-05 13:10:36 -0400

    Faltó corregir el error de los nombres de las funciones scope en el Modelo de Compra.php

--- app/Compra.php
+++ app/Compra.php
@@ -10,22 +10,22 @@ class Compra extends Model
     'user_id','horas_compradas','fecha_compra','monto',
   ];
 
-  public function scopenombre($query, $user_id)
+  public function scopeuser_id($query, $user_id)
   {
     if($user_id)
       return $query->where('user_id', 'LIKE', "%$user_id%");
   }
-  public function scopeapellido($query, $horas_compradas)
+  public function scopehoras_compradas($query, $horas_compradas)
   {
     if($horas_compradas)
       return $query->where('horas_compradas', 'LIKE', "%$horas_compradas%");
   }
-  public function scopecedula($query, $fecha_compra)
+  public function scopefecha_compra($query, $fecha_compra)
   {
     if($fecha_compra)
       return $query->where('fecha_compra', 'LIKE', "%$fecha_compra%");
   }
-  public function scopepasaporte($query, $monto)
+  public function scopemonto($query, $monto)
   {
     if($monto)
       return $query->where('monto', 'LIKE', "%$monto%");
